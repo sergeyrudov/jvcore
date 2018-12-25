db5eb133327897a2747b679a5939ecd4fb2b1397
sergeyrudov <extrazeto@gmail.com>
2018-12-25 21:19:56 +0200

    update

---
+++ jvcore/dayeleven/arrayC.java
@@ -0,0 +1,14 @@
+package jvcore.jvcore.dayeleven;
+
+import java.util.Arrays;
+
+public class arrayC {
+    public static void main(String[] args) {
+        int[][][][] c = new int[2][4][5][5];
+        System.out.println(Arrays.toString(c));
+        System.out.println(Arrays.toString(c[0]));
+        System.out.println(Arrays.toString(c[0][0]));
+        System.out.println(Arrays.toString(c[0][0][0]));
+    }
+
+}
---
+++ jvcore/dayeleven/arraysD.java
@@ -0,0 +1,18 @@
+package jvcore.jvcore.dayeleven;
+
+import java.util.Arrays;
+
+public class arraysD {
+    public static void main(String[] args) {
+        int[][] matrixB = {
+                {-9, 1, 0},
+                {4, 1, 1},
+                {-2, 2, -1}
+        };
+        for (int i = 0; i < matrixB.length; i++) { {
+                System.out.println(Arrays.toString(matrixB[i]));
+            }
+        }
+
+    }
+}
